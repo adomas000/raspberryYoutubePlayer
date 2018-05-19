139df56661c33c113453d58fa23570680ebeebc6
Adomas G <suvumas000@gmail.com>
2018-05-19 13:03:28 +0300

    fixing tests for linux

--- tests/lib/fixYoutubedlDeprecatedErrors.js
+++ tests/lib/fixYoutubedlDeprecatedErrors.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 var path = require('path');
 module.exports = (done) => {
-  var file = path.resolve("node_modules\\youtube-dl\\lib\\youtube-dl.js");
+  var file = path.resolve(path.join('node_modules', 'youtube-dl', 'lib', 'youtube-dl.js'));
   var contents = fs.readFileSync(file, 'utf-8').split("\n");
   if(contents[232].match(/is deprecated/gi)) contents[232] = '';
   if(contents[238].match(/is deprecated/gi)) contents[238] = '';
--- tests/lib/fixYoutubedlNotWorkingOnWin.js
+++ tests/lib/fixYoutubedlNotWorkingOnWin.js
@@ -7,7 +7,7 @@ module.exports = function(done) {
   if(platform != 'win32') return;
   var url = "https://yt-dl.org/downloads/2018.05.18/youtube-dl.exe";
   request(url)
-    .pipe(fs.createWriteStream(path.resolve("node_modules\\youtube-dl\\bin\\youtube-dl.exe")))
+    .pipe(fs.createWriteStream(path.resolve(path.join('node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe'))))
       .on('finish', () => done())
       .on('error', (e) => console.error(e));
 
